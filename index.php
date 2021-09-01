8450b9bbbb73018f6a3754614b0c88419b2a54d1
lalatina-ford <sergeykorotkov590@gmail.com>
2021-09-01 10:16:05 +0300

    Тестовый коммит

--- index.php
+++ index.php
@@ -28,6 +28,7 @@ function mapAssoc($items, $func, $OBJ = null)
     return $results;
 }
 
+
 // Подключение файлов системы
 define('ROOT', dirname(__FILE__));
 require_once(ROOT.'/components/Autoload.php');
